56466f0da24d52f5f7957aa6c95e19d5aa727bd5
Sophie <53999238+SophieOrgana@users.noreply.github.com>
2019-10-11 11:26:03 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function mainStyle() {
+  document.getElementById("changed2").href = "mainStyle.css"
+}
+
+function altStyle() {
+  document.getElementById("changed2").href = "altStyle.css"
+}
+
+function thirdStyle() {
+  document.getElementById("changed2").href = "thirdStyle.css"
+}
+
+// document.getElementById("mainStyle").addEventListener("click", mainstyle())
+//
+// document.getElementById("altStyle").addEventListener("click", altStyle())
+//
+// document.getElementById("thirdStyle").addEventListener("click", thirdstyle())
